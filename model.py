b29fb9dd827063ff6baa1c40f53610d2a05d1223
ZhMJiang <foshanjzm@gmail.com>
2018-10-30 21:12:22 +0800

    Update model.py
    修改Model调用以符合Keras 2 API

--- model.py
+++ model.py
@@ -56,7 +56,7 @@ def unet(pretrained_weights = None,input_size = (256,256,1)):
     conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
     conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
 
-    model = Model(output = [conv10], inputs = [input_s])
+    model = Model(outputs = [conv10], inputs = [input_s])
 
     model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
     
